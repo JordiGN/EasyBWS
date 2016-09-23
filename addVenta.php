5594f438d586c104f0a5bb2e1dbca37ed0b90233
Jordi Alejandro García Núñez <jordign.mx@hotmail.com>
2016-09-23 03:15:48 -0500

    update

--- addVenta.php
+++ addVenta.php
@@ -5,80 +5,44 @@
     global $connect;
 
 
-    $idUsuario = $_POST["idUsuario"];
-    $nombre = $_POST["nombre"];
+    $id_usuario = $_POST["id_usuario"];
+    $idP = $_POST["idP"];
     $precio = $_POST["precio"];
-    $rubroExistente = $_POST["rubroExistente"];
-    $tiporubro = $_POST["tiporubro"];
-    $rubroNuevo = $_POST["rubroNuevo"];
+    $cantidad = $_POST["cantidad"];
+    $modopago = $_POST["modopago"];
+    $deudor = $_POST["deudor"];
+    //$fecha = $_POST["fecha"];
+    $fecha=date('Y-m-d');
 
-   if ($tiporubro=="Existe") {
-   $query = "INSERT INTO catalogo_gastos(id_usuario,id_rubro,nombre, costo ) VALUES ('$idUsuario','$rubroExistente','$nombre','$precio');";
 
-  }else{
-     $query2 = "INSERT INTO rubros(id_usuario,nombre) VALUES ('$idUsuario','$rubroNuevo');";
+    $total=intval($precio)*intval($cantidad);
 
-     mysqli_query($connect,$query2) or die(mysqli_error($connect));
 
-    $query3= "SELECT id_rubro as lastrubro FROM rubros ORDER BY lastrubro  DESC LIMIT 1";
+    if ($modopago=='Contado') {
+      $query="INSERT INTO ventas(id_producto,id_usuario,unidades_vendidas,fecha,modo_pago,total) VALUES ('$idP','$id_usuario','$cantidad','$fecha','0','$total')";
+      mysqli_query($connect,$query) or die(mysqli_error($connect));
+    }else{
+      $query="INSERT INTO ventas(id_producto,id_usuario,unidades_vendidas,fecha,modo_pago,total) VALUES ('$idP','$id_usuario','$cantidad','$fecha','1','$total')";
 
-     $result = mysqli_query($connect,$query3);
+      mysqli_query($connect,$query) or die(mysqli_error($connect));
+      $query3= "SELECT id_venta as lasventa FROM ventas ORDER BY lasventa  DESC LIMIT 1";
 
-     $number_of_rows= mysqli_num_rows($result);
-    $temp_array=array();
-    if ($number_of_rows>0) {
-      while ($row=mysqli_fetch_assoc($result)) {
-          $temp_array[]=$row;
-        # code...
+      $result = mysqli_query($connect,$query3);
+
+      $number_of_rows= mysqli_num_rows($result);
+      $temp_array=array();
+      if ($number_of_rows>0) {
+        while ($row=mysqli_fetch_assoc($result)) {
+            $temp_array[]=$row;
+        }
       }
-      # code...
+      $newRubro2 = $temp_array[0];
+      print_r($newRubro2);
+       $lV=$newRubro2['lasventa'];
+
+      $query2 = "INSERT INTO adeudos(id_usuario,id_venta,deudor, deuda ) VALUES ('$id_usuario','$lV','$deudor','$total');";
+      mysqli_query($connect,$query2) or die(mysqli_error($connect));
     }
-
-    $newRubro2 = $temp_array[0];
-     $newRubro=$newRubro2['lastrubro'];
-
-    $query = "INSERT INTO catalogo_gastos(id_usuario,id_rubro,nombre, costo ) VALUES ('$idUsuario','$newRubro','$nombre','$precio');";
-  }
-
-    mysqli_query($connect,$query) or die(mysqli_error($connect));
     mysqli_close($connect);
   }
  ?>
-
-
-
-
-
-$total=intval($precio)*intval($cantidad);
-
-    if ($modopago=='Contado') {
-      $this->db->set('id_producto',$nombre)//$nombre trae el id del producto
-                ->set('id_usuario',$id_usuario)
-                ->set('unidades_vendidas',$cantidad)
-                ->set('fecha',$fecha)
-                ->set('modo_pago',0)
-                ->set('total',$total)
-                ->insert('ventas');
-    }else{
-      $this->db->set('id_producto',$nombre)//$nombre trae el id del producto
-                ->set('id_usuario',$id_usuario)
-                ->set('unidades_vendidas',$cantidad)
-                ->set('fecha',$fecha)
-                ->set('modo_pago',1)
-                ->set('total',$total)
-                ->insert('ventas');
-
-      $venta=$this->db->select('id_venta as lastventa')
-              ->from('ventas')
-              ->order_by("lastventa","DESC")
-              ->limit(1)
-              ->get()
-              ->result_array();
-      $idventa=$venta[0];
-
-      $this->db->set('id_usuario',$id_usuario)
-                ->set('id_venta',$idventa['lastventa'])
-                ->set('deudor',$deudor)
-                ->set('deuda',$precio*$cantidad)
-                ->insert('adeudos');
-    }
